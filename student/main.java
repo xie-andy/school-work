d4ffe58e4590ba159aa71d312c78577d5fe8bd5b
Andy Xie <andyxie2013@hotmail.com>
2020-07-28 19:26:07 +1000

    changes

--- student/main.java
+++ student/main.java
@@ -1,7 +1,6 @@
 public class main {
 	public static void main(String[] args) {
         Student Andy = new Student("Andy", "Xie", 45253, 1111);
-		//Student Andy = new Student();
 		Andy.markOff("IICH1101");
 	}
 }
--- vehicle/MainProgram.java
+++ vehicle/MainProgram.java
@@ -1,7 +1,6 @@
 public class MainProgram {
 	public static void main(String[] args) {
 		Vehicle myCar = new Vehicle(4, 2005, "Holden", "Captiva");
-		//Vehicle myCar = new Vehicle();
 		myCar.soundHorn();
 		myCar.goForward(500);
 	}
